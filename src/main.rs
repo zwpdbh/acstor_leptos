11e4a2d24550c9d485aa147dab4d0f54a3b3dd09
zwpdbh <zw>
2024-04-13 18:22:21 +0800

    > route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::*;
 
 use components::demo_async::*;
 use components::demo_control_flow::*;
@@ -17,37 +18,51 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     view! {
-        <ul>
-            <li>
-                <BasicComponent/>
-            </li>
-            <li>
-                <ComponentsAndProps/>
-            </li>
-            <li>
-                <DemoBasicIteration/>
-            </li>
-            <li>
-                <DemoComplexDataIteration/>
-            </li>
-            <li>
-                <DemoFormAndInput/>
-            </li>
-            <li>
-                <DemoControlFlow/>
-            </li>
-            <li>
-                <DemoErrorHandling/>
-            </li>
-            <li>
-                <DemoParentChildrenCommunication/>
-            </li>
-            <li>
-                <DemoReactivity/>
-            </li>
-            <li>
-                <DemoAsync/>
-            </li>
-        </ul>
+        <Router>
+            <nav></nav>
+            <main>
+                // all our routes will appear inside <main>
+                <Routes>
+                    <Route path="/demos/basic_component" view=BasicComponent/>
+                    <Route path="/demos/components_and_pros" view=ComponentsAndProps/>
+                    <Route path="/demos/demo_basic_iteration" view=DemoBasicIteration/>
+                    <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
+                </Routes>
+
+            </main>
+        </Router>
     }
 }
+
+// <ul>
+//     <li>
+//         <BasicComponent/>
+//     </li>
+//     <li>
+//         <ComponentsAndProps/>
+//     </li>
+//     <li>
+//         <DemoBasicIteration/>
+//     </li>
+//     <li>
+//         <DemoComplexDataIteration/>
+//     </li>
+//     <li>
+//         <DemoFormAndInput/>
+//     </li>
+//     <li>
+//         <DemoControlFlow/>
+//     </li>
+//     <li>
+//         <DemoErrorHandling/>
+//     </li>
+//     <li>
+//         <DemoParentChildrenCommunication/>
+//     </li>
+//     <li>
+//         <DemoReactivity/>
+//     </li>
+//     <li>
+//         <DemoAsync/>
+//     </li>
+// </ul>
