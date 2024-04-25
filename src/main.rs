f1b3dde39618fc6d389e1b07a19b5a053d08066d
zwpdbh <hyperion_z@outlook.com>
2024-04-25 15:59:04 +0800

    < add nav to the app

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,55 @@ fn main() {
 fn App() -> impl IntoView {
     view! {
         <Router>
-            <nav></nav>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/demos/basic_component">basic components</a>
+                    </li>
+                    <li>
+                        <a href="/demos/components_and_pros">components and props</a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_basic_iteration">basic iterator</a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_form_and_input">form and input</a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_error_handling">error handling</a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_reactivity">reactivity</a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_parent_children_communication">
+                            parent child communication
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/demos/demo_async">demo async</a>
+                    </li>
+                    <li>
+                        <a href="/demos/control_flow">demo control flow</a>
+                    </li>
+                </ul>
+
+            </nav>
             <main>
                 // all our routes will appear inside <main>
                 <Routes>
                     <Route path="/demos/basic_component" view=BasicComponent/>
                     <Route path="/demos/components_and_pros" view=ComponentsAndProps/>
                     <Route path="/demos/demo_basic_iteration" view=DemoBasicIteration/>
+                    <Route path="/demos/demo_form_and_input" view=DemoFormAndInput/>
+                    <Route path="/demos/demo_error_handling" view=DemoErrorHandling/>
+                    <Route path="/demos/demo_reactivity" view=DemoReactivity/>
+                    <Route
+                        path="/demos/demo_parent_children_communication"
+                        view=DemoParentChildrenCommunication
+                    />
+                    <Route path="/demos/demo_async" view=DemoAsync/>
+                    <Route path="/demos/control_flow" view=DemoControlFlow/>
                     <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
                 </Routes>
 
@@ -33,36 +75,3 @@ fn App() -> impl IntoView {
         </Router>
     }
 }
-
-// <ul>
-//     <li>
-//         <BasicComponent/>
-//     </li>
-//     <li>
-//         <ComponentsAndProps/>
-//     </li>
-//     <li>
-//         <DemoBasicIteration/>
-//     </li>
-//     <li>
-//         <DemoComplexDataIteration/>
-//     </li>
-//     <li>
-//         <DemoFormAndInput/>
-//     </li>
-//     <li>
-//         <DemoControlFlow/>
-//     </li>
-//     <li>
-//         <DemoErrorHandling/>
-//     </li>
-//     <li>
-//         <DemoParentChildrenCommunication/>
-//     </li>
-//     <li>
-//         <DemoReactivity/>
-//     </li>
-//     <li>
-//         <DemoAsync/>
-//     </li>
-// </ul>
