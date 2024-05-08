5159816873acfffd0316bfa7534b3e21eb124f63
zwpdbh <hyperion_z@outlook.com>
2024-05-08 16:05:27 +0800

    >> go to server side rendering

--- src/components/demo_nested_route.rs
+++ src/components/demo_nested_route.rs
@@ -27,9 +27,103 @@ pub fn RoutesForDemoNestedRoute() -> impl IntoView {
                 />
 
             </Route>
-
+            <Route path="form_example" view=FormExample/>
             <Route path="" view=|| view! { <p>"Select to see more"</p> }/>
         </Route>
+    }
+}
+
+#[component]
+pub fn FormExample() -> impl IntoView {
+    // reactive access to URL query
+    let query = use_query_map();
+    let name = move || query().get("name").cloned().unwrap_or_default();
+    let number = move || query().get("number").cloned().unwrap_or_default();
+    let select = move || query().get("select").cloned().unwrap_or_default();
+
+    view! {
+        <table>
+            <tr>
+                <td>
+                    <code>"name"</code>
+                </td>
+                <td>{name}</td>
+            </tr>
+            <tr>
+                <td>
+                    <code>"number"</code>
+                </td>
+                <td>{number}</td>
+            </tr>
+            <tr>
+                <td>
+                    <code>"select"</code>
+                </td>
+                <td>{select}</td>
+            </tr>
+        </table>
+
+        // <Form/> will navigate whenever submitted
+        <h2>"Manual Submission"</h2>
+        <Form method="GET" action="">
+            // input names determine query string key
+            <ul>
+                <li>
+                    <input type="text" name="name" value=name/>
+                </li>
+                <li>
+                    <input type="number" name="number" value=number/>
+                </li>
+                <li>
+                    <select name="select">
+                        // `selected` will set which starts as selected
+                        <option selected=move || select() == "A">"A"</option>
+                        <option selected=move || select() == "B">"B"</option>
+                        <option selected=move || select() == "C">"C"</option>
+                    </select>
+                </li>
+            </ul>
+            // submitting should cause a client-side
+            // navigation, not a full reload
+            <input type="submit"/>
+        </Form>
+
+        // This <Form/> uses some JavaScript to submit
+        // on every input
+        <h2>"Automatic Submission"</h2>
+        <Form method="GET" action="">
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        name="name"
+                        value=name
+                        // this oninput attribute will cause the
+                        // form to submit on every input to the field
+                        oninput="this.form.requestSubmit()"
+                    />
+                </li>
+                <li>
+                    <input
+                        type="number"
+                        name="number"
+                        value=number
+                        oninput="this.form.requestSubmit()"
+                    />
+                </li>
+                <li>
+                    <select name="select" onchange="this.form.requestSubmit()">
+                        <option selected=move || select() == "A">"A"</option>
+                        <option selected=move || select() == "B">"B"</option>
+                        <option selected=move || select() == "C">"C"</option>
+                    </select>
+                </li>
+            </ul>
+
+            // submitting should cause a client-side
+            // navigation, not a full reload
+            <input type="submit"/>
+        </Form>
     }
 }
 
@@ -44,6 +138,9 @@ pub fn DemoNestedRoute() -> impl IntoView {
             </li>
             <li>
                 <A href="contacts">"Contacts"</A>
+            </li>
+            <li>
+                <A href="form_example">"Form Example"</A>
             </li>
         </ul>
 
